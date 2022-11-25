1bf8a5ab95ba25ea98bc0f99f7cec61770924d8d
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-11-25 17:01:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+int serie( int nvolte,int contatore,int somma) {
+    int n1 = 1, n2 = 1;
+    for (contatore = 2; contatore<=nvolte; contatore++) {
+        somma = n1 + n2;
+        cout << somma << endl;
+        n1 = n2;
+        n2 = somma;
+    }
+    return n1;
+}
 
 int main() {
+    int nvolte,contatore,somma,n1=1,n2=1;
+    cin>>nvolte;
+        cout<<n1<<endl<<n2<<endl;
+        serie(nvolte,contatore, somma);
    return 0;
 }
